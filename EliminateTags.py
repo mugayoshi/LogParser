3804bd6926996aa4ce644167fcb5931609e5882d
muga <muga.y.j3039@gmail.com>
2014-12-12 12:33:48 +0900

    Add Functions
    - add makeSentence, getIDAndTag, checkID
    - make array of ID and check it when this program read ‘After’ tag log

--- EliminateTags.py
+++ EliminateTags.py
@@ -36,9 +36,43 @@ def filter(line, argID):
     else:
         return '0'
     
+def  makeSentence(line):
+    if line.find('Modify'):
+        arry = line.split(':')
+        hasID = 0
+        method_top = 0
+        log = ''
+        string = []
+        for x in arry:
+            
+            if x.find('ID') != -1:
+                hasID = 1
 
+            elif x.find('CLASS') != -1:
+                method_top = 1;
+                              
+            if (hasID == 1 or method_top == 1):
+                log += x
+                
+        return log
+    else:
+        return ''
  
+def getIDAndTag(line):
+    log = []
+    log = line.split()
+#    print(log)
+    ID = log[0]
+    tag = log[2]
+    return (ID, tag)
 
+def checkID(arry, ID):
+    if len(arry) < 1:
+        return 0
+    for i in arry:
+        if i == ID:
+            return 1
+    return 0
 
 def main():
     current = os.path.dirname(os.path.realpath(__file__))
@@ -51,9 +85,24 @@ def main():
             
     f = open(file)
     line = f.readline()
-    ID = 0
+    IDs = []
+
     while line:
-        ID = filter(line, ID)
+        #ID = filter(line, ID)
+        tab_str = ''
+        log = makeSentence(line)
+        if len(log) > 1:
+            ID, tag = getIDAndTag(log)
+            if tag == 'Before':
+                IDs.append(ID)
+            for i in range(len(IDs)):
+                tab_str += '\t'
+                
+            print(tab_str + log)
+            if tag == 'After':
+                if checkID(IDs, ID) == 1:
+                    IDs.remove(ID)
+           
         line = f.readline()
     f.close
 
