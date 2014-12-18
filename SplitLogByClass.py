b036f8ee00ca4ac66fb17d8fe24ead4aa11f9a70
muga <muga.y.j3039@gmail.com>
2014-12-18 13:46:35 +0900

    Update SplitByClass
    - prints line number of its original file
    - makes a directory and move generated files to the directory
    - deletes a condition in refine()
    - at first, shows  a list of log text files and you choose one of them

--- SplitLogByClass.py
+++ SplitLogByClass.py
@@ -1,10 +1,9 @@
 from __future__ import print_function
-import glob
 import os
-
 import re
-import string
-   
+import time
+import shutil
+
 def  makeSentence(line):
     if line.find('Modify'):
         if (line.find('ID') == -1 and line.find('CLASS') == -1):
@@ -39,7 +38,7 @@ def getIDAndTag(line):
         if x == "CLASS":
             return (0,"CLASS")
         i += 1
-    print(i)
+#    print(i)
     ID = log[i + 1]
     tag = log[i + 2]
     
@@ -87,37 +86,45 @@ def refine(file):
     output = open(outputFile, 'w')
     while line:
         tab_str = ''
-        log = makeSentence(line)
-        if len(log) > 1:
+        s = line.split(":")
+        lineNum = s[0]
+        slice1 = s[1:]
+        log = ''.join(slice1)
 
-            ID, tag = getIDAndTag(log)
-            if tag == 'Before':
-                IDs.append(ID)
-            for i in range(len(IDs)):
-                tab_str += '\t'
-            output.write(tab_str +  log)
+        ID, tag = getIDAndTag(log)
+        if tag == 'Before':
+            IDs.append(ID)
+        for i in range(len(IDs)):
+            tab_str += '\t'
+        output.write(lineNum + tab_str +  log)
 
-            if tag == 'After':
-                if checkID(IDs, ID) == 1:
-                    IDs.remove(ID)
+        if tag == 'After':
+            if checkID(IDs, ID) == 1:
+                IDs.remove(ID)
 
         line = f.readline()
 
     f.close
+    return outputFile
     
 def main():
     current = os.path.dirname(os.path.realpath(__file__))
+    txt_files = []
     for file in os.listdir(current):
         if (file.endswith(".txt") and 'imatch' in file):
-            print("Do you wanna see " + file + "?")
-            input_ans = raw_input()
-            if (input_ans.find('y') != -1 or input_ans.find('Y') != -1):
-                break
-            
+            txt_files.append(file)
+    i = 0
+    print("Enter The Number")
+    for t in txt_files:
+        print(str(i) + ": " + t)
+        i += 1
+
+    num = raw_input()
+    file = txt_files[int(num)]
     f = open(file)
     print("Writing Start!")
     line = f.readline()
-#    IDs = []
+
     lineNum = 0
     outputs = []
     while line:
@@ -130,14 +137,20 @@ def main():
                 outputs.append(outputFile)
             output = open(outputFile, 'a+')
             output.write(str(lineNum) + ":" +  log)
+
             lineNum += 1
         line = f.readline()
 
     f.close
 
+    date = time.strftime("%H_%M_%d_%m_%Y")
+    newDirPath = os.getcwd() + "/SplitByClass_" + date
+    os.mkdir(newDirPath, 0755)
     for x in outputs:
-        refine(x)
+        output = refine(x)
         os.remove(x)
+        src = os.getcwd() + "/" + output
+        shutil.move(src, newDirPath)
 
     print("Writing Done!")
 
